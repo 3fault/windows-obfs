c30faa943ff1f904a22b58c38b5e70ad7a6ee134
vd4re <vd4re@protonmail.ch>
2023-12-07 22:53:26 -0500

    add resolver module

---
+++ src/resolver.rs
@@ -0,0 +1,9 @@
+use crate::windows_fn::WindowsFn;
+
+pub struct Resolver;
+
+impl Resolver {
+    pub fn resolve_fn<F>(&self, _module: &str, _name: &str) -> WindowsFn<F> {
+        unimplemented!()
+    }
+}
